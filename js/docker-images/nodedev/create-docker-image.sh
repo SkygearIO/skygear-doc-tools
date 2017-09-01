799af357ffb4fe924c7fdfdaa4e8ebe0dccf3d97
Ho Wa <howa@oursky.com>
2017-09-01 19:32:30 +0800

    JS doc tool added

---
+++ js/docker-images/nodedev/create-docker-image.sh
@@ -0,0 +1,7 @@
+DOCKER_ORG_NAME="skygeario"
+DOCKER_IMAGE="skygear-nodedev"
+DOCKER_TAG="latest"
+
+IMAGE_NAME=$DOCKER_ORG_NAME/$DOCKER_IMAGE:$DOCKER_TAG
+DIR=`dirname $0`
+docker build -t $IMAGE_NAME $DIR
