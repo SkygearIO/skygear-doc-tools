69e163c7f816981dfe3a35113ec7b0d82569f5de
Ho Wa <howa@oursky.com>
2017-11-22 14:25:43 +0800

    Add Python Documentation Tool

--- setup.py
+++ setup.py
@@ -8,11 +8,12 @@ setup(
     version='0.0.1',
     zip_safe=False,
     include_package_data=True,
-    install_requires=['click'],
+    install_requires=['click', 'Sphinx', 'sphinx-rtd-theme', 'sphinxcontrib-napoleon'],
     packages=find_packages(),
     scripts=['bin/create-docker-image',
              'bin/generate-js-doc',
              'bin/generate-ios-doc',
              'bin/generate-android-doc',
+             'bin/generate-python-doc',
              'bin/publish-doc'],
 )
